a60426e0f597adaeb874c6a833b6a28b852215cc
hafiar <hafidzfirman.a@gmail.com>
2024-11-08 14:49:46 +0700

    commit pertama

---
+++ resources/views/admin/comments/show.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.admin')
+@section('content')
+
+<div class="card">
+    <div class="card-header">
+        {{ trans('global.show')  }} {{ trans('cruds.comment.title') }}
+    </div>
+
+    <div class="card-body">
+        <div class="mb-2">
+            <table class="table table-bordered table-striped">
+                <tbody>
+                    <tr>
+                        <th>
+                            {{ trans('cruds.comment.fields.id') }}
+                        </th>
+                        <td>
+                            {{ $comment->id }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            {{ trans('Title') }}
+                        </th>
+                        <td>
+                            {{ $comment->ticket->title ?? '' }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            {{ trans('Report Name') }}
+                        </th>
+                        <td>
+                            {{ $comment->author_name }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            {{ trans('Email Name') }}
+                        </th>
+                        <td>
+                            {{ $comment->author_email }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            {{ trans('cruds.comment.fields.user') }}
+                        </th>
+                        <td>
+                            {{ $comment->user->name ?? '' }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            {{ trans('cruds.comment.fields.comment_text') }}
+                        </th>
+                        <td>
+                            {!! $comment->comment_text !!}
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <a style="margin-top:20px;" class="btn btn-default" href="{{ url()->previous() }}">
+                {{ trans('global.back_to_list') }}
+            </a>
+        </div>
+
+
+    </div>
+</div>
+@endsection
